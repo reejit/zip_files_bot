97f58c46f19bee4cc8f9ba23215b4d1829ea035c
reejit <70316178+reejit@users.noreply.github.com>
2021-02-08 09:49:53 +0530

    Update __main__.py

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -1,1 +1,21 @@
-Ho
+import glob
+from pathlib import Path
+from AccountGen.utils import load_plugins
+import logging
+from . import lolboyisback
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.WARNING)
+
+path = "Bot/plugins/*.py"
+files = glob.glob(path)
+for name in files:
+    with open(name) as a:
+        patt = Path(a.name)
+        plugin_name = patt.stem
+        load_plugins(plugin_name.replace(".py", ""))
+    
+print("Successfully deployed Compress Bot!")
+
+if __name__ == "__main__":
+    lolboyisback.run_until_disconnected()
